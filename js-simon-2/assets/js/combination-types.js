a4013358deb861e647e8a4e6afde693fe155f54b
fedebacci <federicobacci1999@gmail.com>
2025-04-14 16:49:48 +0200

    aggiunta di colori e modifica opzioni possibili per le difficolta dei colori

--- js-simon-2/assets/js/combination-types.js
+++ js-simon-2/assets/js/combination-types.js
@@ -23,18 +23,25 @@ const combinationTypes = [
         name: 'Colori',
         value: 'colors',
         options: [
-            '#ff0000',      // red           rgb(255, 0, 0)
+            '#8b0000',      // darkred       rgb(139, 0, 0)
+            '#cd5c5c',      // indianred     rgb(205, 92, 92)
             '#ffff00',      // yellow        rgb(255, 255, 0)
+            '#fffacd',      // lemonchiffon  rgb(255, 250, 205)
             '#0000ff',      // blue          rgb(0, 0, 255)
+            '#add8e6',      // lightblue     rgb(173, 216, 230)
             '#008000',      // green         rgb(0, 128, 0)
+            '#90ee90',      // lightgreen    rgb(144, 238, 144)
+            '#00ff00',      // lime          rgb(50, 205, 50)
             '#964b00',      // brown         rgb(150, 75, 0)
             '#ffa500',      // orange        rgb(255, 165, 0)
             '#800080',      // purple        rgb(128, 0, 128)
+            '#ff00ff',      // magenta       rgb(255, 0, 255)
             '#ffc0cb',      // pink          rgb(255, 192, 203)
+            '#ff69b4',      // hotpink       rgb(255, 105, 180)
             '#000000',      // black         rgb(0, 0, 0)
             '#ffffff',      // white         rgb(255, 255, 255)
             '#808080',      // gray          rgb(128, 128, 128)
-            '#add8e6',      // lightblue     rgb(173, 216, 230)
+            '#d3d3d3',      // lightgray     rgb(211, 211, 211)
             '#deb887'       // burlywood     rgb(222, 184, 135)
         ]
     }
