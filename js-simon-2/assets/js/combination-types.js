4d52c7fea8a13a01b5840e1ab336159e9119357a
fedebacci <federicobacci1999@gmail.com>
2025-04-14 16:29:37 +0200

    documentazione delle opzioni dei colori

--- js-simon-2/assets/js/combination-types.js
+++ js-simon-2/assets/js/combination-types.js
@@ -23,49 +23,19 @@ const combinationTypes = [
         name: 'Colori',
         value: 'colors',
         options: [
-            '#ff0000',
-            '#ffff00',
-            '#0000ff',
-            '#008000',
-            '#964b00',
-            '#ffa500',
-            '#800080',
-            '#ffc0cb',
-            '#000000',
-            '#ffffff',
-            '#808080',
-            '#add8e6',
-            '#deb887'
-        ],
-        // options: [
-        //     'red',
-        //     'yellow',
-        //     'blue',
-        //     'green',
-        //     'brown',
-        //     'orange',
-        //     'purple',
-        //     'pink',
-        //     'black',
-        //     'white',
-        //     'gray',
-        //     'lightblue',
-        //     'burlywood'
-        // ],
-        // options: [
-        //     'rgb(255, 0, 0)',
-        //     'rgb(255, 255, 0)',
-        //     'rgb(0, 0, 255)',
-        //     'rgb(0, 128, 0)',
-        //     'rgb(150, 75, 0)',
-        //     'rgb(255, 165, 0)',
-        //     'rgb(128, 0, 128)',
-        //     'rgb(255, 192, 203)',
-        //     'rgb(0, 0, 0)',
-        //     'rgb(255, 255, 255)',
-        //     'rgb(128, 128, 128)',
-        //     'rgb(173, 216, 230)',
-        //     'rgb(222, 184, 135)'
-        // ],
+            '#ff0000',      // red           rgb(255, 0, 0)
+            '#ffff00',      // yellow        rgb(255, 255, 0)
+            '#0000ff',      // blue          rgb(0, 0, 255)
+            '#008000',      // green         rgb(0, 128, 0)
+            '#964b00',      // brown         rgb(150, 75, 0)
+            '#ffa500',      // orange        rgb(255, 165, 0)
+            '#800080',      // purple        rgb(128, 0, 128)
+            '#ffc0cb',      // pink          rgb(255, 192, 203)
+            '#000000',      // black         rgb(0, 0, 0)
+            '#ffffff',      // white         rgb(255, 255, 255)
+            '#808080',      // gray          rgb(128, 128, 128)
+            '#add8e6',      // lightblue     rgb(173, 216, 230)
+            '#deb887'       // burlywood     rgb(222, 184, 135)
+        ]
     }
 ];
